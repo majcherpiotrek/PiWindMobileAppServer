263e77d19ec1e85ca9d9ac38ef17c5d3ec44d06e
Piotr Majcher <piotr.majcher94@gmail.com>
2017-11-14 12:37:30 +0100

    Added handling the meteo data update for push notifications

--- src/main/java/com/piotrmajcher/piwind/mobileappserver/services/AndroidPushNotificationsService.java
+++ src/main/java/com/piotrmajcher/piwind/mobileappserver/services/AndroidPushNotificationsService.java
@@ -1,10 +1,15 @@
 package com.piotrmajcher.piwind.mobileappserver.services;
 
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 import org.springframework.http.HttpEntity;
 
+import com.piotrmajcher.piwind.mobileappserver.web.dto.MeteoDataTOAndroid;
+
 public interface AndroidPushNotificationsService {
 	
 	CompletableFuture<String> send(HttpEntity<String> entity);
+	
+	void handleMeteoDataUpdate(UUID stationId, MeteoDataTOAndroid updatedMeteoData);
 }
--- src/main/java/com/piotrmajcher/piwind/mobileappserver/services/impl/AndroidPushNotificationsServiceImpl.java
+++ src/main/java/com/piotrmajcher/piwind/mobileappserver/services/impl/AndroidPushNotificationsServiceImpl.java
@@ -2,9 +2,13 @@ package com.piotrmajcher.piwind.mobileappserver.services.impl;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -13,6 +17,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.piotrmajcher.piwind.mobileappserver.services.AndroidPushNotificationsService;
+import com.piotrmajcher.piwind.mobileappserver.services.MeteoStationService;
+import com.piotrmajcher.piwind.mobileappserver.services.exceptions.MeteoStationServiceException;
+import com.piotrmajcher.piwind.mobileappserver.web.dto.MeteoDataTOAndroid;
+import com.piotrmajcher.piwind.mobileappserver.web.dto.MeteoStationTO;
 import com.piotrmajcher.piwind.mobileappserver.web.rest.HeaderRequestInterceptor;
 
 @Service
@@ -22,10 +30,12 @@ public class AndroidPushNotificationsServiceImpl implements AndroidPushNotificat
 	private static final String FIREBASE_API_URL = "https://fcm.googleapis.com/fcm/send";
 	
 	private RestTemplate restTemplate;
+	private MeteoStationService meteoStationService;
 	
 	@Autowired
-	public AndroidPushNotificationsServiceImpl() {
+	public AndroidPushNotificationsServiceImpl(MeteoStationService meteoStationService) {
 		this.restTemplate = new RestTemplate();
+		this.meteoStationService = meteoStationService;
 	}
 	
 	@Async
@@ -45,6 +55,63 @@ public class AndroidPushNotificationsServiceImpl implements AndroidPushNotificat
 		String firebaseResponse = restTemplate.postForObject(FIREBASE_API_URL, entity, String.class);
  
 		return CompletableFuture.completedFuture(firebaseResponse);
+	}
+
+	@Override
+	public void handleMeteoDataUpdate(UUID stationId, MeteoDataTOAndroid updatedMeteoData) {
+		
+		if (updatedMeteoData.getWindSpeed() > 1.0) {
+			
+			try {
+				MeteoStationTO station = meteoStationService.getStation(stationId);
+				JSONObject body = createNotificationBody(station, updatedMeteoData);
+				
+				HttpEntity<String> request = new HttpEntity<>(body.toString());
+				CompletableFuture<String> pushNotification = send(request);
+				CompletableFuture.allOf(pushNotification).join();
+				String firebaseResponse = pushNotification.get();
+				logger.info("Notification for station with id " + stationId + " has been sent!");
+				logger.info("Firebase response: " + firebaseResponse);
+			} catch (ExecutionException | InterruptedException | JSONException | MeteoStationServiceException e) {
+				logger.error("Error while trying to send the notification: " + e.getMessage());
+			}
+		}
+	}
+	
+	private JSONObject createNotificationBody(MeteoStationTO station, MeteoDataTOAndroid updatedMeteoData) throws JSONException {
+		/**
+		{
+		   "notification": {
+		      "title": "<station_name>",
+		      "body": "The wind has picked up!"
+		   },
+		   "data": {
+		      "station_id": "<station_uuid>",
+		      "name": "<station_name>",
+		      "stationBaseURL": "<station_url> //not necessary probably
+		   },
+		   "to": "/topics/<station_uuid>",
+		   "priority": "high"
+		}
+*/
+		JSONObject body = new JSONObject();
+		body.put("to", "/topics/" + station.getId().toString());
+		body.put("priority", "high");
+ 
+		JSONObject notification = new JSONObject();
+		notification.put("title", station.getName());
+		notification.put("body", "The wind has picked up!");
+		notification.put("sound", "default");
+		
+		JSONObject data = new JSONObject();
+		data.put("id", station.getId().toString());
+		data.put("name", station.getName());
+		data.put("stationBaseURL", station.getStationBaseURL());
+ 
+		body.put("notification", notification);
+		body.put("data", data);
+		
+		return body;
 	}
 
 }
